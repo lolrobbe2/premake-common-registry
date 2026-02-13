05a6a267efa62d0703a25bf0f3d38b6442a6a113
lolrobbe2 <90089980+lolrobbe2@users.noreply.github.com>
2026-02-13 17:27:32 +0100

    add utf8cpp (#6)

---
+++ libraries/nemtrif/utfcpp/premake5.lua
@@ -0,0 +1,13 @@
+project "utf8cpp"
+	language    "C++"
+    kind        "None"  
+	warnings    "Off"
+    includedirs 
+    {
+        "source"
+    }
+    files 
+    { 
+        "source/**.h", 
+    }
+    
