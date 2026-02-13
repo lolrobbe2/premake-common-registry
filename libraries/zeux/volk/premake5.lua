4b01ef8422adc61935821a3a5fa29410124b4914
Robbe Beernaert <robbebeernaert@hotmail.be>
2026-02-13 17:11:31 +0100

    add volk

---
+++ libraries/zeux/volk/premake5.lua
@@ -0,0 +1,24 @@
+project "volk"
+	language    "C"
+    kind        "StaticLib"  
+	warnings    "Off"
+    defines {
+        "_LIB",
+        "API_NAME=\"vulkan\"",
+    }
+    includedirs {
+        ".",
+    }
+    files {
+        "volk.c",
+        "volk.h",
+    }
+
+    filter "platforms:Windows"
+        defines {
+            "VK_USE_PLATFORM_WIN32_KHR",
+        }
+    filter("platforms:Linux")
+        defines {
+            "VK_USE_PLATFORM_XCB_KHR",
+        }
